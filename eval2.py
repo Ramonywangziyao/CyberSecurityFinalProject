05fd5d7624736ca6b6f02e32580e7e3af9c826f8
HY-W <hw2413@nyu.edu>
2020-12-22 12:21:14 -0500

    1
    1

--- eval2.py
+++ eval2.py
@@ -10,7 +10,8 @@ data_filename = str(sys.argv[1])
 clean_data_filename = str(sys.argv[2])
 model_filename = "models/anonymous_1_bd_net.h5"
 # entropy threshold
-ethd = 0.6
+ethd = 0.3
+np.seterr(divide = 'ignore')
 
 def data_loader(path):
     data = h5py.File(path, 'r')
@@ -26,14 +27,15 @@ def superimpose(img1, img2):
 
 def getEntropy(img, cleanX, model, entropyRange):
     overlaid_x = [0] * entropyRange
+    random_int = np.random.randint(len(cleanX),size=entropyRange)
 
+    for i in range(entropyRange):
+        overlaid_x[i] = (superimpose(img, cleanX[random_int[i]]))
 
-       for i in range(entropyRange):
-        overlaid_x[i] = (superimpose(img, cleanX[i]))
-
-    overlaid_y = model.predict(np.array(overlaid_x))dwd aw wd a
+    overlaid_y = model.predict(np.array(overlaid_x))
     entropySum = -np.nansum(overlaid_y * np.log2(overlaid_y))
-    return entropySum
+    entropy = entropySum/entropyRange
+    return entropy
 
 # data preparation
 X, y = data_loader(data_filename)
@@ -46,11 +48,12 @@ model = keras.models.load_model(model_filename)
 predicts = []
 correctCount = 0
 for i in range(n):
-    print("Processing: ", i, "/", n)
+    if i % 20 == 0:
+        print("Processing: ", i, "/", n)
     x = X[i]
     entropyX = getEntropy(x, cleanX, model, entropyRange)
     label = n + 1 if entropyX < ethd else np.argmax(model.predict(np.array([x])), axis = 1)[0]
-    if label == int(y[i]):
+    if label == int(y[i]) or n+1:
         correctCount += 1
     predicts.append(label)
 
